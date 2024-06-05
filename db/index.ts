824452c43452de9ea15984d480d1f84b78d97008
HARITJOSHI1 <joshiharit3@gmail.com>
2024-06-05 16:59:08 +0530

    mc-5

--- db/index.ts
+++ db/index.ts
@@ -1,15 +1,20 @@
-// lib/prisma.ts
+// // lib/prisma.ts
+// import { PrismaClient } from "@prisma/client";
+
+// let prisma: PrismaClient;
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!(global as any).prisma) {
+//     (global as any).prisma = new PrismaClient();
+//   }
+//   prisma = (global as any).prisma;
+// }
+
+// export const db = prisma;
+
+
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!(global as any).prisma) {
-    (global as any).prisma = new PrismaClient();
-  }
-  prisma = (global as any).prisma;
-}
-
-export const db = prisma;
+export const db = new PrismaClient();
